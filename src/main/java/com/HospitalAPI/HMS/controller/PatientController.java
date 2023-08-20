f79b5f959dc7854cac92c48a7f8bec3f4ce33262
teledi-makhafola <lkgahliso03@gmail.com>
2023-08-20 19:00:29 +0200

    added delete method

--- src/main/java/com/HospitalAPI/HMS/controller/PatientController.java
+++ src/main/java/com/HospitalAPI/HMS/controller/PatientController.java
@@ -3,10 +3,7 @@ package com.HospitalAPI.HMS.controller;
 import com.HospitalAPI.HMS.model.Patient;
 import com.HospitalAPI.HMS.repository.PatientRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 // allows the class to handle the requests made by the client.
@@ -15,12 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 // used to map web requests onto specific handler classes and/or handler methods
 public class PatientController {
 
-
     PatientRepository patientRepository;
 
     @PostMapping("/add")
     public ResponseEntity<Object> addPatient(@RequestBody Patient patient){
         patientRepository.save(patient);
         return ResponseEntity.ok("Successfully added patient");
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Object> deletePatient(@PathVariable Long id){
+        patientRepository.deleteById(id);
+        return ResponseEntity.ok("Successfully deleted patient");
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Object> updatePatient(@PathVariable Long id){
+
+        return null;
     }
 }
--- src/main/java/com/HospitalAPI/HMS/repository/PatientRepository.java
+++ src/main/java/com/HospitalAPI/HMS/repository/PatientRepository.java
@@ -4,7 +4,9 @@ import com.HospitalAPI.HMS.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-
+    Optional<Patient> findById(Long id);
 }
